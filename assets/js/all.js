3b72a56db275dc9847f4d6952e3912710d9cacf7
Key <springfield0329@gmail.com>
2022-08-23 01:10:54 +0800

    Update 2022-08-22T17:10:49.031Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,48 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+// header
+// $(document).ready(function(){
+//   $(window).on('wheel', function(event){
+//     if(event.originalEvent.deltaY > 0){
+//       $("header").removeClass("bg-transparent").addClass("bg-secondary");
+//     }
+//   });
+// });
+// swiper
+var swiper1 = new Swiper('.swiper1', {
+  // Optional parameters
+  // direction: 'horizontal',
+  // loop: true,
+  freeMode: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
