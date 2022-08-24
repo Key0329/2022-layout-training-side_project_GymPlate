92fc37b35626de0465372c096386c6caf241bea1
Key <springfield0329@gmail.com>
2022-08-25 02:51:07 +0800

    Update 2022-08-24T18:51:04.010Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -68,5 +68,44 @@ var index__swiper__pt = new Swiper(".index__swiper__pt", {
       spaceBetween: 40
     }
   }
+}); // AOS
+
+AOS.init({
+  // Global settings:
+  disable: false,
+  // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded',
+  // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init',
+  // class applied after initialization
+  animatedClassName: 'aos-animate',
+  // class applied on animation
+  useClassNames: false,
+  // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false,
+  // disables automatic mutations' detections (advanced)
+  debounceDelay: 50,
+  // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99,
+  // the delay on throttle used while scrolling the page (advanced)
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 600,
+  // values from 0 to 3000, with step 50ms
+  easing: 'ease',
+  // default easing for AOS animations
+  once: false,
+  // whether animation should happen only once - while scrolling down
+  mirror: false,
+  // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+}); // scrollspy
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-feature'
 });
 //# sourceMappingURL=all.js.map
